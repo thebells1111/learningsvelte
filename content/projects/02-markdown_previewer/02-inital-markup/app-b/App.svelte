dc3201bd2c3a50ebbe036a9c3a70ce13e02c5f17
thebells1111 <thebells1111@gmail.com>
2020-07-28 13:50:49 -0500

    work on markdown_previewer chap 2

---
+++ content/projects/02-markdown_previewer/02-inital-markup/app-b/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import marked from 'marked';
+  let markdown = '# Marked in browser\n\nRendered by **marked**.';
+  $: content = marked(markdown);
+</script>
+
+<textarea id="editor" bind:value={markdown} />
+<div id="preview">
+  {@html content}
+</div>
---
+++ content/projects/02-markdown_previewer/02-inital-markup/text.md
@@ -0,0 +1,5 @@
+---
+title: Create Initial Markup
+---
+
+
